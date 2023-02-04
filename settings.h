42eda0feda2d8eec7b177ded475be121e77b60fc
Slave2k <114007371+Slave2k@users.noreply.github.com>
2023-02-04 19:33:52 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+const float WINDOW_WIDTH = 800.f;
+const float WINDOW_HEIGHT = 1000.f;
+const std::string WINDOW_TITLE = "Racing S";
+const float FPS = 60.f;
+
+
+
